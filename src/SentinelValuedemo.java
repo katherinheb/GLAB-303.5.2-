0e17ce265b88192edb7933246a1907b4767483f4
Blanco <katherinhe.blanco@accenture.com>
2024-11-08 13:50:00 -0500

    first commit

---
+++ src/SentinelValuedemo.java
@@ -0,0 +1,9 @@
+
+public class SentinelValuedemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/SubtractionQuizLoop.java
@@ -0,0 +1,9 @@
+
+public class SubtractionQuizLoop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
